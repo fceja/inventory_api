b3f22075441bdd2084df3fc92d81b5817776813f
fceja <88167774+fceja@users.noreply.github.com>
2024-03-07 13:19:24 -0800

    implement joi validation

--- src/controllers/ProductsController.ts
+++ src/controllers/ProductsController.ts
@@ -1,10 +1,23 @@
 import { Request, Response } from "express";
+import Joi from "joi";
 
 import { createProductsMidW } from "@middleware/products/CreateProductsMidW";
 import { deleteByProductsIdMidW } from "@middleware/products/DeleteByProductsIdMidW";
 import { getProductsMidW } from "@middleware/products/GetProductsMidW";
 import { ProductsModelI } from "@models/ProductsModel";
 import { updateByProductsIdMidW } from "@middleware/products/UpdateByProductsIdMidw";
+
+// define validation schema for products model props
+const ProductsModel = Joi.object({
+  // optional string prop
+  description: Joi.string().optional(),
+
+  // optional string prop, cannot be empty if provided
+  name: Joi.string().optional().required().empty(""),
+
+  // optional numeric prop, cannot be a negative number if provided
+  quantity: Joi.number().min(0).optional(),
+}).strict();
 
 // CREATE operations
 export const createProducts = async (req: Request, res: Response) => {
@@ -51,12 +64,12 @@ export const deleteByProductsId = async (req: Request, res: Response) => {
 // UPDATE operations
 export const updateByProductsId = async (req: Request, res: Response) => {
   try {
-    const { productsId } = req.params;
-    const productsData: ProductsModelI = req.body;
+    const { error } = ProductsModel.validate(req.body);
+    if (error) throw new Error(error.message);
 
     const success = await updateByProductsIdMidW(
-      Number(productsId),
-      productsData,
+      parseInt(req.params.productsId),
+      req.body,
     );
     if (!success) throw new Error("Error updating.");
 
--- src/middleware/products/UpdateByProductsIdMidw.ts
+++ src/middleware/products/UpdateByProductsIdMidw.ts
@@ -4,7 +4,7 @@ import { connPool } from "@db/DbPoolClient";
 
 import { ProductsModelI } from "@models/ProductsModel";
 
-// region - helpers
+// helper
 const generateQuery = (productsId: number, productsData: ProductsModelI) => {
   // generate SET clause and params for query
   const setClausePairs = [];
@@ -19,27 +19,13 @@ const generateQuery = (productsId: number, productsData: ProductsModelI) => {
 
   // final query
   const query = `
-  UPDATE products A
-  SET ${querySetClause}, updated_at = CURRENT_TIMESTAMP
-  WHERE A.products_id = $${queryParams.length}
+    UPDATE products A
+    SET ${querySetClause}, updated_at = CURRENT_TIMESTAMP
+    WHERE A.products_id = $${queryParams.length}
   `;
 
   return { query: query, queryParams: queryParams };
 };
-
-const validateModelProps = (productsData: ProductsModelI) => {
-  // using type coercion to check for null or undefined values
-  if (productsData.hasOwnProperty("name") && productsData.name == null)
-    throw new Error("'name' prop cannot be null | undefined");
-  if (
-    productsData.hasOwnProperty("quantity") &&
-    (productsData.quantity == null || productsData.quantity < 0)
-  )
-    throw new Error(
-      "'quantity' prop cannot be null | undefined | less than zero",
-    );
-};
-// endregion - helpers
 
 export const updateByProductsIdMidW = async (
   productsId: number,
@@ -48,9 +34,6 @@ export const updateByProductsIdMidW = async (
   let dbConn: PoolClient | null = null;
 
   try {
-    // validate model props
-    validateModelProps(productsData);
-
     // generate query
     const { query, queryParams } = generateQuery(productsId, productsData);
 
