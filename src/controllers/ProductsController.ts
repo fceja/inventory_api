3fc491354d8429575c14f9ffdc0b9854f5407376
fceja <88167774+fceja@users.noreply.github.com>
2024-03-08 10:50:35 -0800

    simplify create

--- src/controllers/ProductsController.ts
+++ src/controllers/ProductsController.ts
@@ -3,7 +3,6 @@ import { Request, Response } from "express";
 import { createProductsMidW } from "@middleware/products/CreateProductsMidW";
 import { deleteByProductsIdMidW } from "@middleware/products/DeleteByProductsIdMidW";
 import { getProductsMidW } from "@middleware/products/GetProductsMidW";
-import { ProductsModelI } from "@models/ProductsModel";
 import { updateByProductsIdMidW } from "@middleware/products/UpdateByProductsIdMidw";
 
 // CREATE operations
@@ -16,7 +15,7 @@ export const createProducts = async (req: Request, res: Response) => {
   } catch (error) {
     console.error(error.message);
 
-    res.status(400).json({ success: false });
+    res.status(500).json({ success: false, message: "Internal Server Error." });
   }
 };
 
@@ -29,16 +28,18 @@ export const getProducts = async (_req: Request, res: Response) => {
   } catch (error) {
     console.error(error.message);
 
-    res.status(400).json({ success: false });
+    res.status(500).json({ success: false, message: "Internal Server Error." });
   }
 };
 
-// DELETE operations
-export const deleteByProductsId = async (req: Request, res: Response) => {
+// UPDATE operations
+export const updateByProductsId = async (req: Request, res: Response) => {
   try {
-    const { productsId } = req.params;
-
-    const success = await deleteByProductsIdMidW(Number(productsId));
+    const success = await updateByProductsIdMidW(
+      parseInt(req.params.productsId),
+      req.body,
+    );
+    if (!success) throw new Error("Error updating.");
 
     res.status(200).json({ success: true });
   } catch (error) {
--- src/middleware/products/CreateProductsMidW.ts
+++ src/middleware/products/CreateProductsMidW.ts
@@ -2,56 +2,50 @@ import { PoolClient } from "pg";
 
 import { connPool } from "@db/DbPoolClient";
 import { ProductsModelI } from "@db/models/ProductsModel";
+import { camelToSnake } from "@utils/StringUtils";
 
-export const createProductsMidW = async (productInfo: ProductsModelI) => {
+const generateQuery = (productsData: ProductsModelI) => {
+  // generate sql query statement
+  const productsProps = [];
+  const valuesClause = [];
+  const queryParams = [];
+  Object.entries(productsData).forEach(([key]) => {
+    const snakeCaseString = camelToSnake(key);
+    productsProps.push(snakeCaseString);
+    valuesClause.push(`$${queryParams.length + 1}`);
+    queryParams.push(productsData[key]);
+  });
+
+  const querySetClause = valuesClause.join(", ");
+  const queryProductsProps = productsProps.join(", ");
+
+  // final query
+  const query = `
+    INSERT INTO products (${queryProductsProps})
+    VALUES (${querySetClause})
+  `;
+
+  return { query: query, queryParams: queryParams };
+};
+
+export const createProductsMidW = async (productsData: ProductsModelI) => {
   let dbConn: PoolClient | null = null;
 
   try {
-    // NOTE - does not contain the following since they are generated automatically by db
-    // product_id
-    // created_at,
-    // updated_at,
-    const qResult = await connPool.query(
-      `
-        INSERT INTO products (
-          bar_code,
-          categories_id,
-          depth,
-          description,
-          fragile,
-          heavy,
-          height,
-          name,
-          qr_code,
-          quantity,
-          refrigerated,
-          sku_code,
-          weight,
-          width
-        )
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
-    `,
-      [
-        productInfo.barCode ?? null,
-        productInfo.categoriesId ?? null,
-        productInfo.depth ?? null,
-        productInfo.description ?? null,
-        productInfo.fragile ?? null,
-        productInfo.heavy ?? null,
-        productInfo.height ?? null,
-        productInfo.name,
-        productInfo.qrCode ?? null,
-        productInfo.quantity,
-        productInfo.refrigerated ?? null,
-        productInfo.skuCode ?? null,
-        productInfo.weight ?? null,
-        productInfo.width ?? null,
-      ],
-    );
+    /**
+     * NOTE - the following are generated automatically by db
+     * product_id
+     * created_at,
+     * updated_at,
+     */
 
-    if (qResult.rowCount > 0) return true;
+    const { query, queryParams } = generateQuery(productsData);
 
-    throw new Error("Error inserting.");
+    const qResult = await connPool.query(query, queryParams);
+
+    if (qResult.rowCount == 1) return true;
+
+    throw new Error("Error creating.");
   } catch (error) {
     console.error(error.message);
 
