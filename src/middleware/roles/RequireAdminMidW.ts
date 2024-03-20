c5f722555ae9ab0eb74fdb672cf76793df5fc6aa
fceja <88167774+fceja@users.noreply.github.com>
2024-03-20 09:53:10 -0700

    update admin role midw to reflect session changes

--- src/middleware/roles/RequireAdminMidW.ts
+++ src/middleware/roles/RequireAdminMidW.ts
@@ -1,20 +1,26 @@
 import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
 
 export default function requireAdminMidW(
   req: Request,
   res: Response,
   next: NextFunction,
 ) {
-  // verify user has admin role
   try {
-    if (!(req.session.systemUser.role === "admin")) {
+    // retrieve role from decoded payload
+    const jwtToken = req.headers["jwt-token"] as string;
+    const decodedToken = jwt.decode(jwtToken) as { [key: string]: any };
+    const role = decodedToken.role;
+
+    // verify user has admin role
+    if (!(role === "admin")) {
       throw new Error("User not authorized.");
     }
 
-    // user is authorized, pass request to next function
     next();
   } catch (error) {
     console.error(error.message);
+
     return res.status(401).send({ success: false, message: "Not authorized." });
   }
 }
