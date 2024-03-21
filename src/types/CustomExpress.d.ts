a55118113f6115adfac43246962feee231eb4364
fceja <88167774+fceja@users.noreply.github.com>
2024-03-21 08:31:13 -0700

    remove express session

--- src/types/CustomExpress.d.ts
+++ src/types/CustomExpress.d.ts
@@ -1,18 +1,4 @@
-import "express-session";
 import { PoolClient } from "pg";
-
-interface SystemUserSessionData {
-  systemUserId: number | null;
-  email: string | null;
-  role: string | null;
-  token: string | null;
-}
-
-declare module "express-session" {
-  export interface SessionData {
-    systemUser?: SystemUserSessionData;
-  }
-}
 
 declare global {
   namespace Express {
